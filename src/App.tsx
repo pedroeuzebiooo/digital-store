b1963b9fd12a90f6e3c043e240f1466e6dcee21a
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-23 15:36:45 -0300

    style: remove text base title featured collections section 2

--- src/App.tsx
+++ src/App.tsx
@@ -79,9 +79,7 @@ export function App() {
       </section>
 
       <section className="p-5 bg-light-gray-3 space-y-5">
-        <h2 className="text-base text-dark-gray-2 font-bold">
-          Coleções em destaque
-        </h2>
+        <h2 className="text-dark-gray-2 font-bold">Coleções em destaque</h2>
 
         <div className="grid grid-cols-2 justify-center items-center gap-5">
           <div className="flex flex-col justify-center items-center gap-3">
