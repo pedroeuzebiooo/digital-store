a20afbd438d67feaa26051685652fac7b3ffb562
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-23 12:23:17 -0300

    style: update colors from variables colors

--- src/App.tsx
+++ src/App.tsx
@@ -13,55 +13,55 @@ export function App() {
   return (
     <div className="w-96 mx-auto">
       <header className="flex items-center justify-between p-5">
-        <Menu className="size-6 text-[#1F1F1F]" />
+        <Menu className="size-6" />
 
         <a href="#">
           <img src={Logo} alt="Logo do Tennis Store" />
         </a>
 
         <div className="flex items-center gap-3 relative">
-          <Search className="size-6 text-[#CCCCCC]" />
-          <ShoppingCart className="size-6 text-[#C92071]" />
+          <Search className="size-6 text-light-gray-2" />
+          <ShoppingCart className="size-6 text-primary" />
 
-          <span className="flex items-center justify-center w-4 h-4 p-1 rounded-full bg-[#EE4266] text-xs text-white font-bold absolute right-[-0.25rem] top-[-0.25rem]">
+          <span className="flex items-center justify-center w-4 h-4 p-1 rounded-full bg-error text-xs text-white font-bold absolute right-[-0.25rem] top-[-0.25rem]">
             2
           </span>
         </div>
       </header>
 
-      <section className="p-5 bg-[#F5F5F5] space-y-8">
+      <section className="p-5 bg-light-gray-3 space-y-8">
         <img src={WhiteSneakers} alt="White Sneakers" />
 
-        <p className="text-sm text-[#C92071] text-center font-bold">
+        <p className="text-sm text-primary text-center font-bold">
           Melhores ofertas personalizadas
         </p>
 
-        <h1 className="text-5xl text-[#1F1F1F] text-center font-bold">
+        <h1 className="text-5xl text-center font-bold">
           Queima de estoque Nike 🔥
         </h1>
 
-        <p className="text-sm text-[#474747] text-center">
+        <p className="text-sm text-dark-gray-2 text-center">
           Consequat culpa exercitation mollit nisi excepteur do do tempor
           laboris eiusmod irure consectetur.
         </p>
 
         <button
           type="button"
-          className="flex justify-center items-center bg-[#C92071] text-sm text-[#F5F5F5] font-bold w-full rounded-lg p-3"
+          className="flex justify-center items-center bg-primary text-sm text-light-gray-3 font-bold w-full rounded-lg p-3"
         >
           Ver Ofertas
         </button>
 
         <div className="flex justify-center items-center gap-3">
-          <div className="w-3 h-3 bg-[#C92071] rounded-full"></div>
-          <div className="w-3 h-3 bg-[#CCCCCC] rounded-full"></div>
-          <div className="w-3 h-3 bg-[#CCCCCC] rounded-full"></div>
-          <div className="w-3 h-3 bg-[#CCCCCC] rounded-full"></div>
+          <div className="w-3 h-3 bg-primary rounded-full"></div>
+          <div className="w-3 h-3 bg-light-gray-2 rounded-full"></div>
+          <div className="w-3 h-3 bg-light-gray-2 rounded-full"></div>
+          <div className="w-3 h-3 bg-light-gray-2 rounded-full"></div>
         </div>
       </section>
 
-      <section className="p-5 bg-[#F5F5F5] space-y-3">
-        <h2 className="text-base text-[#474747] font-bold">
+      <section className="p-5 bg-light-gray-3 space-y-3">
+        <h2 className="text-base text-dark-gray-2 font-bold">
           Coleções em destaque
         </h2>
 
@@ -74,8 +74,8 @@ export function App() {
         </div>
       </section>
 
-      <section className="p-5 bg-[#F5F5F5] space-y-5">
-        <h2 className="text-base text-[#474747] font-bold">
+      <section className="p-5 bg-light-gray-3 space-y-5">
+        <h2 className="text-base text-dark-gray-2 font-bold">
           Coleções em destaque
         </h2>
 
@@ -84,28 +84,28 @@ export function App() {
             <div className="p-5 bg-white rounded-full text-center">
               <img src={Camiseta} alt="Camiseta" />
             </div>
-            <p className="text-xs text-[#474747] font-bold">Camisetas</p>
+            <p className="text-xs text-dark-gray-2 font-bold">Camisetas</p>
           </div>
 
           <div className="flex flex-col justify-center items-center gap-3">
             <div className="p-5 bg-white rounded-full text-center">
               <img src={Calca} alt="Calça" />
             </div>
-            <p className="text-xs text-[#474747] font-bold">Calças</p>
+            <p className="text-xs text-dark-gray-2 font-bold">Calças</p>
           </div>
 
           <div className="flex flex-col justify-center items-center gap-3">
             <div className="p-5 bg-white rounded-full text-center">
               <img src={Headphone} alt="Headphone" />
             </div>
-            <p className="text-xs text-[#474747] font-bold">Headphones</p>
+            <p className="text-xs text-dark-gray-2 font-bold">Headphones</p>
           </div>
 
           <div className="flex flex-col justify-center items-center gap-3">
             <div className="p-5 bg-white rounded-full text-center">
               <img src={Tenis} alt="Tênis" />
             </div>
-            <p className="text-xs text-[#474747] font-bold">Tênis</p>
+            <p className="text-xs text-dark-gray-2 font-bold">Tênis</p>
           </div>
         </div>
       </section>
