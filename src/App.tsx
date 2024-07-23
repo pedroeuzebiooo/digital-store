44c497594f35598d775ccac1f461b22e5557eea3
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-23 15:34:43 -0300

    style: add space y wrap images featured collections section 1

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ export function App() {
       <section className="p-5 bg-light-gray-3 space-y-5">
         <h2 className="text-dark-gray-2 font-bold">Coleções em destaque</h2>
 
-        <div className="flex flex-col justify-center items-center gap-4">
+        <div className="flex flex-col justify-center items-center gap-5">
           <img src={newDropSupreme} alt="Novo Drop Supreme" />
 
           <img src={adidasCollection} alt="Coleção Adidas" />
