bb6aa05177aa75a05db6779fd12775c275a542f7
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-07-23 15:44:01 -0300

    style: add gap 16px wrap category images featured collections section 2

--- src/App.tsx
+++ src/App.tsx
@@ -82,28 +82,28 @@ export function App() {
         <h2 className="text-dark-gray-2 font-bold">Coleções em destaque</h2>
 
         <div className="grid grid-cols-2 justify-center items-center gap-5">
-          <div className="flex flex-col justify-center items-center gap-3">
+          <div className="flex flex-col justify-center items-center gap-4">
             <div className="p-5 bg-white rounded-full text-center">
               <img src={Camiseta} alt="Camiseta" />
             </div>
             <p className="text-xs text-dark-gray-2 font-bold">Camisetas</p>
           </div>
 
-          <div className="flex flex-col justify-center items-center gap-3">
+          <div className="flex flex-col justify-center items-center gap-4">
             <div className="p-5 bg-white rounded-full text-center">
               <img src={Calca} alt="Calça" />
             </div>
             <p className="text-xs text-dark-gray-2 font-bold">Calças</p>
           </div>
 
-          <div className="flex flex-col justify-center items-center gap-3">
+          <div className="flex flex-col justify-center items-center gap-4">
             <div className="p-5 bg-white rounded-full text-center">
               <img src={Headphone} alt="Headphone" />
             </div>
             <p className="text-xs text-dark-gray-2 font-bold">Headphones</p>
           </div>
 
-          <div className="flex flex-col justify-center items-center gap-3">
+          <div className="flex flex-col justify-center items-center gap-4">
             <div className="p-5 bg-white rounded-full text-center">
               <img src={Tenis} alt="Tênis" />
             </div>
